e76715e2632e290b955162fe52ff39ebd49aec17
WetPirat1 <126335909+WetPirat1@users.noreply.github.com>
2023-08-30 22:36:33 +0300

    full adaptation

--- main-js/javascript.js
+++
@@ -1,7 +0,0 @@
-let menuBtn = document.querySelector('.burger-menu');
-let menu = document.querySelector('.burger-item');
-
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-})
